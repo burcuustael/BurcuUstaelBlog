8d0794203d099225bacc2992ec74055da3af16d6
burcuustael <burcustael@gmail.com>
2022-11-10 21:10:46 +0300

    Add project files.

---
+++ BurcuUstaelBlog/Controllers/IletisimController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BurcuUstaelBlog.Controllers
+{
+    public class IletisimController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormCollection collections)
+        {
+            ViewBag.Name = collections["_name"];
+            ViewBag.Email = collections["email"];
+            ViewBag.Mesaj = collections["mesaj"];
+            ViewBag.Kontrol = collections["kontrol"][0];
+
+            return View();
+        }
+    }
+}
---
+++ BurcuUstaelBlog/Controllers/OgrendiklerimController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BurcuUstaelBlog.Controllers
+{
+    public class OgrendiklerimController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Asama2()
+        {
+            return View();
+        }
+
+
+    }
+}
